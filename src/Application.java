b8868de82546c1d41e48eaaeb01ac2f415359d27
Valentin Vedeshkin <evilfunt@gmail.com>
2016-06-13 21:07:57 +0300

    Adding history,experemental.

--- src/Application.java
+++ src/Application.java
@@ -1,6 +1,11 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 /**
  * Created by vedeshkin on 14.05.2016.
@@ -14,13 +19,23 @@ public class Application extends JFrame {
     private JTextArea TimeSignatureOut;
     private JLabel History;
     private JComboBox history;
+    private static Logger logger;
+    private static FileHandler fh;
 
     public Application () {
         super("TechnicalMetalGenerator");
         setContentPane(mainPanel);
+        System.setProperty("java.util.logging.SimpleFormatter.format","[%1$tc] %4$s:%5$s%n");
+        try{
+            fh = new FileHandler("MetalGeneratorLog.log");
+            fh.setFormatter(new SimpleFormatter());
+        }catch (IOException ex){}
+        logger = Logger.getLogger(Application.class.getName());
+        logger.addHandler(fh);
         final MusicGenerator gen = new MusicGenerator();
         pack();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        logger.info("Application has been  started");
         generateButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -29,6 +44,7 @@ public class Application extends JFrame {
                 TimeSignatureOut.setText(gen.getTimeSignature());
                 ScaleOut.setText(gen.getScale());
                 history.addItem(gen);
+                logger.info(gen.toString());
 
 
 
